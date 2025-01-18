4bb3c73e4bd092b6a143f24c7c310eccee2742cd
rafaelabou1999 <rafaelabourdette@gmail.com>
2025-01-18 11:58:10 -0300

    :wrench: Adding vercel.json

--- public/index.php
+++ public/index.php
@@ -1,4 +1,4 @@
-<?php include __DIR__ . "/../src/inc/Header.inc.php"?>
+<?php require_once __DIR__ . "/../src/inc/Header.inc.php"?>
    
     <?php include __DIR__ . "/../src/Views/old_post.php"?>
     <div class="pages">
