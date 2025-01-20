d81636b8c8948a7afce03a5efeefb200f74d457f
rafaelabou1999 <rafaelabourdette@gmail.com>
2025-01-20 20:11:43 -0300

    :art: Improving code by adding posts_info

--- src/Views/blog.php
+++ src/Views/blog.php
@@ -1,22 +1,12 @@
 <?php
-include __DIR__ . '/../inc/functions.inc.php';
-include __DIR__ . '/../inc/sinopse.inc.php';
+include_once __DIR__ . '/../inc/functions.inc.php';
 
 $is_blog = true;
-$title = null;
+include __DIR__ . '/../inc/posts_info.inc.php';
+
+$title = $_GET['title'];
 $isHome = false; 
 
-if(isset($_GET['title'])){
-    $title = $_GET['title'];
-    $posts = [
-        "Ainda estou aqui" => "../../public/assets/ainda-estou-aqui.webp",
-        "Wicked" => "../../public/assets/wicked.jpeg",
-        "Duna 2" => "../../public/assets/duna-2.png",
-        "A substância" => "../../public/assets/substancia.jpg",
-    ]; 
-
-    if(array_key_exists($title, $posts)) $image = $posts[$title];  
-} 
 ?>
 
 <!DOCTYPE html>
@@ -29,61 +19,64 @@ if(isset($_GET['title'])){
     <title><?= e($title) ?></title>
 </head>
 <body>
-<?php if($title):?>
-    <?php if(array_key_exists($title,$posts) && isset($posts[$title])):?>
-        <?php include __DIR__ . "/../inc/Header.inc.php"?>
-        <div class="has_title">
-            <div class="path_display"><a href="../../public/index.php">Home</a> 🢚 <span>Blog</span></div>
-            <div class="img_banner">
-                <h1 class="blog_title"><?= e($title) ?></h1>
-                <figure>
-                    <img class="blog_banner" src="<?= e($image);?>"/>
-                    <figcaption>| Image by xyz</figcaption>
-                </figure>
-            </div>
-            <!--<div class="star">4.9</div>!-->
-            <div class="line"></div>
-            <div class="sinopse_box">
-        
-                <?php foreach ($sinopse AS $title_sinopse => $text){
-                        if($title_sinopse === $title){
-                            $text_array = explode("\n", $text);
-                        } 
-                    }   
-                ?>
+<?php
+$title = trim($_GET['title']);
+$found = false;
 
-                <p class='sinopse'><?php echo join("</p><br><p class='sinopse'>", $text_array)?></p><br>
-            </div>
-            <div class="author_box">
-                    <img class="author" src="../../public/assets/profile-user.jpeg"/>
-                    <div>
-                        <h4>Texto gerado por IA</h4>
-                        <p>Jan 18, 2025</p>
+foreach ($posts as $post) {
+    if (isset($post['title']) && trim($post['title']) === $title) {
+        $found = true;
+        switch ($title) {
+            case $post['title']:
+                include __DIR__ . "/../inc/Header.inc.php"; ?>
+                <div class="has_title">
+                    <div class="path_display"><a href="../../public/index.php">Home</a> 🢚 <span>Blog</span></div>
+                    <div class="img_banner">
+                        <h1 class="blog_title"><?= e($post['title']) ?></h1>
+                        <figure>
+                            <img class="blog_banner" src="<?= e($post['image']);?>"/>
+                            <figcaption>| Image by xyz</figcaption>
+                        </figure>
+                    </div>
+                    <div class="line"></div>
+                    <div class="sinopse_box">
+                        <?php $text_array = explode("\n", $post['sinopse']); ?>
+                        <p class='sinopse'><?php echo join("</p><br><p class='sinopse'>", $text_array)?></p><br>
+                    </div>
+                    <div class="author_box">
+                        <img class="author" src="../../public/assets/profile-user.jpeg"/>
+                        <div>
+                            <h4>Texto gerado por IA</h4>
+                            <p>Jan 18, 2025</p>
+                        </div>
+                    </div>
+                    <form action="blog.php">
+                        <div class="input_blog">
+                            <label class="comment_title">Comentarios</label>
+                            <input type="text" name="comment" class="comment">
+                        </div>
+                    </form>
+                    <div class="comment_section">
+                        <h5 class="comment_text">Filme maravilhoso!!!!!! Vale a pena assistir</h5>
                     </div>
                 </div>
-                
-            <form action="blog.php">
-                <div class="input_blog">
-                    <label class="comment_title">Comentarios</label>
-                    <input type="text" name="comment" class="comment">
-                </div>
-            </form>
-            <div class="comment_section">
-            <h5 class="comment_text">Filme maravilhoso!!!!!! Vale a pena assistir</h5>
-            </div>
-        </div> 
-    <?php else: ?>
-        <div class="post_error">
-            <h4>Título não encontrado!</h4>
-            <button><a href="../../public/index.php">Home</a></button>
-        </div> 
-    <?php endif; ?>
-<?php else: ?>
+                <?php
+                break;
+            default:
+                break;
+        }
+    }
+}
+
+if (!$found) {
+    ?>
     <div class="post_error">
-        <h4>Erro. Url inválido!</h4>
+        <h4>Título não encontrado!</h4>
         <button><a href="../../public/index.php">Home</a></button>
     </div> 
-<?php endif; ?>
+    <?php
+}
+?>
 </body>
 
 
--- src/Views/posts.php
+++ src/Views/posts.php
@@ -1,38 +1,27 @@
 <?php
-include __DIR__ . '/../inc/functions.inc.php';
-include __DIR__ . '/../inc/sinopse.inc.php';
+include_once __DIR__ . '/../inc/functions.inc.php';
+include __DIR__ . '/../inc/posts_info.inc.php';
 
-$posts = [
-    "Ainda estou aqui" => "../public/assets/ainda-estou-aqui.webp",
-    "Wicked" => "../public/assets/wicked.jpeg",
-    "Duna 2" => "../public/assets/duna-2.png",
-    "A substância" => "../public/assets/substancia.jpg",
-];
-
+$is_blog = false;
 $posts_first = array_slice($posts, 0, -1, true);
 $posts_second = array_slice($posts, -1, 1, true);
 
-$sinopse_post = array_slice(array_values($sinopse), 0, 1, false);
 ?>
 
 <div class="all_posts">
     <div class="first_page">
-        <?php foreach ($posts_first AS $title => $image):?>
+        <?php foreach ($posts_first AS $post):?>
             <?php
-                $movies = ['title' => $title];
+                $movies = ['title' => $post['title']] ;
                 $query = http_build_query($movies);
                 $url = "../src/Views/blog.php?" . $query;
             ?>
             <div class="post">
-                <img class="post_card_img" src="<?= e($image);?>"/>
+                <img class="post_card_img" src="<?= e($post['image']);?>"/>
                 <div class="right_part_card">
-                    <h3 class="post_card_title"><?= e($title); ?></h3>  
+                    <h3 class="post_card_title"><?= e($post['title']); ?></h3>  
                     <h4 class="genero">★★★★★</h4> 
-                    <?php foreach ($sinopse AS $sinopse_title => $p):?>
-                        <?php if ($title === $sinopse_title):?>
-                            <p><?php echo substr($p, 0 ,250);?></p>     
-                        <?php endif; ?>
-                    <?php endforeach; ?>
+                    <p><?php echo substr($post['sinopse'], 0 ,250);?>...</p>     
                     <button class="post_card_btn" onclick="window.location.href='<?= $url ?>'">Leia mais</button>
                 </div>
             
@@ -41,22 +30,18 @@ $sinopse_post = array_slice(array_values($sinopse), 0, 1, false);
     </div>
       
     <div class="second_page">
-        <?php foreach ($posts_second AS $title => $image):?>
+        <?php foreach ($posts_second AS $post):?>
             <?php
-                $movies = ['title' => $title];
+                $movies = ['title' => $post['title']] ;
                 $query = http_build_query($movies);
                 $url = "../src/Views/blog.php?" . $query;
              ?>
             <div class="post">
-                <img class="post_card_img" src="<?= e($image);?>"/>
+                <img class="post_card_img" src="<?= e($post['image']);?>"/>
                 <div class="right_part_card">
-                <h3 class="post_card_title"><?= e($title); ?></h3>  
+                <h3 class="post_card_title"><?= e($post['title']); ?></h3>  
                 <h4 class="genero">★★★★★</h4> 
-                <?php foreach ($sinopse AS $sinopse_title => $p):?>
-                        <?php if ($title === $sinopse_title):?>
-                            <p><?php echo substr($p, 0 ,250);?></p>     
-                        <?php endif; ?>
-                <?php endforeach; ?>
+                <p><?php echo substr($post['sinopse'], 0 ,250);?>...</p>     
                 <button class="post_card_btn" onclick="window.location.href='<?= $url ?>'">Leia mais</button>
              </div>
             </div>
